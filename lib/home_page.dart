6d598963e8d5b1cbe2ab58429feea872da44be01
crystall.101600 <crystall.101600@gmail.com>
2025-02-13 09:36:55 +0000

    Contains all the UI of the app.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,56 +1,64 @@
 import 'package:flutter/material.dart';
+import 'story_brain.dart';
 
-class HomePage extends StatefulWidget{
-    _HomePageState createState() => _HomePageState();
+StoryBrain storyBrain = StoryBrain();
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
-    @override
-    Widget build(BuildContext context){
-        return Scaffold(
-            body: Container(
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage(
-                            'images/background.png',
-                        ),
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage('images/background.png')),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 15),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    storyBrain.getStory(),
                     ),
+                  ),
+              ),
+              Expanded(
+                flex: 2,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    storyBrain.getChoice1(),
+                  ),
                 ),
-                padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 15,),
-                constraints: BoxConstraints.expand(),
-                child: SafeArea(
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                            Expanded(
-                                flex: 12,
-                                child: Center(
-                                    child: Text(),
-                                ),
-                            ),
-                            Expanded(
-                                flex: 2,
-                                child: ElevatedButton(
-                                    color: Colors.red,
-                                    onPressed: (){},
-                                    child: Text(),
-                                ),
-                            ),
-                            const SizedBox(
-                                height: 20,
-                            ),
-                            Expanded(
-                                flex: 2,
-                                child: ElevatedButton(
-                                    color: Colors.blue,
-                                    onPressed: (){},
-                                    child: Text(),
-                                ),
-                            ),
-                        ],
-                    ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                flex: 2,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                  onPressed: () {},
+                  child: Text(
+                    storyBrain.getChoice2(),
+                  ),
                 ),
-            ),
-        );
-    }
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
